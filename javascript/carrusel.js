2aa79cf2bfe223a18b15c49bd8387b893eb3de6c
RubenU2002 <rsantiagourrego@gmail.com>
2022-09-26 16:45:29 -0500

    carrusel de pelis

--- javascript/carrusel.js
+++ javascript/carrusel.js
@@ -7,10 +7,17 @@ if(screen.width <=485){
         autoplay: true,
         arrows: false,
     } ).mount();
+    new Splide('#slider2',{
+        arrows: false,
+        pagination:false,
+    } ).mount();
 }
 else{
     console.log(screen.width)
     new Splide( '.splide',{
         autoplay: true,
+    } ).mount();
+    new Splide( '#slider2',{
+        arrows: false,
     } ).mount();
 }
